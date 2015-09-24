c70a2888f99b5b8a1fb44f67b3b1cc5f7d777f55
David Stark <david@starkers.org>
2015-09-24 11:41:36 +0100

    notes and provisional script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# archbook
+# Overview
 Install arch onto an HP chromebook 11
+
+
+Kudos to @ https://github.com/omgmog/archarm-usb-hp-chromebook-11
+but based off the steps here: http://archlinuxarm.org/platforms/armv7/samsung/samsung-chromebook
+
+I got tired of all the typing so posting here
+
+on chromeos ensure you have :
+- developer mode enabled
+- run ```dev_install```
+- you installed parted: ```emerge parted```
+
+
---
+++ splat.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+#per:  http://archlinuxarm.org/platforms/armv7/samsung/samsung-chromebook
+
+
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { echo "$@" ; "$@" 1>/dev/null 2>/dev/null || die "cannot $*"; }
+
+
+if [ X$1 == X ]; then
+  DISK=/dev/sda
+else
+  DISK=$1
+fi
+
+echo "Installing to [$DISK] in 5 seconds... ctrl+c to abort"
+sleep 5
+
+for a in ${DISK}* ; do
+umount $a
+done
+set -e
+
+
+try dd if=/dev/zero of="$DISK" bs=1M count=30
+try parted "$DISK" mklabel gpt
+try cgpt create "$DISK"
+try cgpt add -i 1 -t kernel -b 8192 -s 32768 -l Kernel -S 1 -T 5 -P 10 "$DISK"
+
+SECTOR="$(cgpt show $DISK | grep "Sec GPT table" | awk '{print $1}')"
+
+try cgpt add -i 2 -t data -b 40960 -s `expr $SECTOR - 40960` -l Root "$DISK"
+try sync
+sleep 1
+if grep -q /dev/sd <<<"$DISK" ; then
+  # I assume this is the USB stick and I'm inside chromeos
+  try sfdisk -R $DISK
+fi
+
+if grep -q /dev/mmcblk <<<"$DISK" ; then
+  # I assume this is now the inbuilt MMC and I'm inside arch
+  try partprobe
+fi
+
+sleep 1
+
+try mkfs.ext4 -L root -m 0 "${DISK}2"
+try wget http://archlinuxarm.org/os/ArchLinuxARM-peach-latest.tar.gz -c
+
+try mkdir -p root
+try mount "${DISK}2" root
+try tar -xf ArchLinuxARM-peach-latest.tar.gz -C root
+
+try dd if=root/boot/vmlinux.kpart of="${DISK}1"
+try sync
+try umount root
+
+yell reboot, ctrl+u
+exit
+
+
